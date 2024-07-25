b0895b56db175a355742a73e295d969bda108da6
Angel Jhonel Pesantes Romero <104596032+Jhonel890@users.noreply.github.com>
2024-07-24 19:57:39 -0500

    Se actualiza el menú

--- src/app/components/NavigationMenu.jsx
+++ src/app/components/NavigationMenu.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import {
     AppBar,
@@ -22,17 +22,14 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import LockIcon from "@mui/icons-material/Lock";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import HistoryIcon from "@mui/icons-material/History";
 import { useAuth } from "@/context/AuthContext";
-import GetAppIcon from '@mui/icons-material/GetApp';
-import { ADMIN_ROLE_NAME, RESEARCHER_ROLE_NAME } from "@/constants";
-import LoginIcon from '@mui/icons-material/Login';
-import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 const NavigationMenu = () => {
-    let { user, logoutUser, loginUser } = useAuth();
+    let { user, logoutUser } = useAuth();
     const router = useRouter();
     const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
-    // user = { role: { name: "Administrador" } }
+    user = { role: { name: "Administrador" } }
 
     const handleMobileMenuOpen = () => {
         setMobileMenuOpen(true);
@@ -44,7 +41,7 @@ const NavigationMenu = () => {
 
     const handleLogout = () => {
         logoutUser();
-        router.push("/");
+        router.push("/auth/login");
         handleMobileMenuClose(); // Cerrar menú después de cerrar sesión
     };
 
@@ -58,40 +55,23 @@ const NavigationMenu = () => {
         { label: "Mi Perfil", icon: <PersonIcon />, path: "/users/me" }
     ];
 
-    const menuItemsNoUser = [
-        { label: "Dashboard", icon: <DashboardIcon />, path: "/" },
-        { label: "Registrarse", icon: <PersonAddAltIcon />, path: "/auth/register" },
-        { label: "Iniciar sesión", icon: <LoginIcon />, path: "/auth/login" }
-    ];
-
-    useEffect(() => {
-        if (!user) {
-            const userData = window.localStorage.getItem("user")
-            const token = window.localStorage.getItem("token")
-
-            // Si ya hay sesión, logueo al usuario, sino, lo mando al login
-            if (userData && token) {
-                loginUser(JSON.parse(userData), token)
-            }
-        }
-    }, []);
-
-    if (user?.role.name === ADMIN_ROLE_NAME) {
+    if (user?.role.name === "Administrador") {
         menuItems.push(
             { label: "Administradores", icon: <PeopleIcon />, path: "/users/admins" },
-            { label: "Investigadores", icon: <PeopleIcon />, path: "/researchers" },
-            { label: "Nodos", icon: <WifiIcon />, path: "/nodes" },
+            { label: "Investigadores", icon: <PeopleIcon />, path: "/users/researchers" },
+            { label: "Usuarios", icon: <PeopleIcon />, path: "/users/usersApp"},
+            { label: "Nodos", icon: <WifiIcon />, path: "/monitoringStation/node/createNode" },
             { label: "Estaciones de monitoreo", icon: <PlaceIcon />, path: "/monitoringStation" },
+            { label: "Historial", icon: <HistoryIcon />, path: "/monitoringStation/sensors/history" },
             { label: "Alertas", icon: <NotificationsIcon />, path: "/alerts" },
-            { label: "Solicitudes de descarga", icon: <LockIcon />, path: "/download-request" },
-            { label: "Sensores", icon: <WifiIcon />, path: "/sensors" },
+            { label: "Solicitudes de descarga", icon: <LockIcon />, path: "/application-form" },
+            { label: "Sensores", icon: <WifiIcon />, path: "/monitoringStation/sensors/createSensors" },
             { label: "Límites de Seguridad", icon: <SettingsIcon />, path: "/system-settings/security-limits" },
             { label: "Actividades del Sistema", icon: <SettingsIcon />, path: "/system-settings/system-activities" }
         );
-    } else if (user?.role.name === RESEARCHER_ROLE_NAME) {
+    } else if (user?.role.name === "Investigador") {
         menuItems.push(
-            { label: "Mis Solicitudes", icon: <NotificationsIcon />, path: "/my-request" },
-            { label: "Solicitar datos", icon: <GetAppIcon />, path: "/access-requests/create" }
+            { label: "Mis Solicitudes", icon: <NotificationsIcon />, path: "/dashboard" }
         );
     }
 
@@ -102,27 +82,18 @@ const NavigationMenu = () => {
                 open={mobileMenuOpen}
                 onClose={handleMobileMenuClose}
             >
-                {user ?
-                    <List>
-                        {menuItems.map((item) => (
-                            <ListItem button key={item.label} onClick={() => handleNavigation(item.path)}>
-                                <ListItemIcon>{item.icon}</ListItemIcon>
-                                <ListItemText primary={item.label} />
-                            </ListItem>
-                        ))}
-                        <ListItem button onClick={handleLogout}>
-                            <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-                            <ListItemText primary="Cerrar Sesión" />
+                <List>
+                    {menuItems.map((item) => (
+                        <ListItem button key={item.label} onClick={() => handleNavigation(item.path)}>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.label} />
                         </ListItem>
-                    </List> :
-                    <List>
-                        {menuItemsNoUser.map((item) => (
-                            <ListItem button key={item.label} onClick={() => handleNavigation(item.path)}>
-                                <ListItemIcon>{item.icon}</ListItemIcon>
-                                <ListItemText primary={item.label} />
-                            </ListItem>
-                        ))}
-                    </List>}
+                    ))}
+                    <ListItem button onClick={handleLogout}>
+                        <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+                        <ListItemText primary="Cerrar Sesión" />
+                    </ListItem>
+                </List>
             </Drawer>
             <AppBar position="static">
                 <Toolbar>
