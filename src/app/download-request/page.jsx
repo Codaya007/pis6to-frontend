8a3c572db043842dfd952196eac9c106c9d41a06
vivi <viviana.calva@lumation.co>
2024-07-26 21:47:14 -0500

    Updating buttons

--- src/app/download-request/page.jsx
+++ src/app/download-request/page.jsx
@@ -39,15 +39,15 @@ export default function DownloadRequests() {
 
     const handleViewDownloadRequest = (id) => {
         // Lógica para actualizar el solicitud de descarga downloadrequestistrador
-        router.push(`/downloadrequests/view/${id}`);
+        router.push(`/download-request/view/${id}`);
     };
 
-    const handleUpdateDownloadRequestStatus = async (id, state) => {
+    const handleAcceptRequest = async (id) => {
         try {
-            await updateDownloadRequestById(id, { state }, token);
+            await updateDownloadRequestById(token, id, { status: "Aceptada" });
             await getDownloadRequests();
 
-            mensajes("Éxito", "Solicitud de descarga actualizado exitosamente", "info");
+            mensajes("Éxito", "Solicitud de descarga aceptada exitosamente", "info");
         } catch (error) {
             console.log(error)
             console.log(error?.response?.data || error.message);
@@ -56,27 +56,19 @@ export default function DownloadRequests() {
         }
     }
 
-    // const handleDeleteDownloadRequest = async (id) => {
-    //     // Lógica para dar de baja al solicitud de descarga downloadrequestistrador
-    //     console.log(`Dando de baja al downloadrequestistrador con ID: ${id}`);
+    const handleDennyRequest = async (id) => {
+        try {
+            await updateDownloadRequestById(token, id, { status: "Denegada" },);
+            await getDownloadRequests();
 
-    //     // setDownloadRequests(downloadrequests.filter(downloadrequest => downloadrequest_.id !== id));
-    //     MensajeConfirmacion("Esta acción es irreversible. ¿Desea continuar?", "Confirmación", "warning").then(async () => {
-    //         try {
-    //             await deleteDownloadRequestById(token, id);
-    //             await getDownloadRequests();
+            mensajes("Éxito", "Solicitud de descarga denegada exitosamente", "info");
+        } catch (error) {
+            console.log(error)
+            console.log(error?.response?.data || error.message);
 
-    //             mensajes("Éxito", "Solicitud de descarga eliminado exitosamente", "info");
-    //         } catch (error) {
-    //             console.log(error)
-    //             console.log(error?.response?.data || error.message);
-
-    //             mensajes("Error en eliminación", error.response?.data?.customMessage || "No se ha podido eliminar el solicitud de descarga", "error");
-    //         }
-    //     }).catch((error) => {
-    //         console.error(error);
-    //     })
-    // };
+            mensajes("Error en actualización", error.response?.data?.customMessage || "No se ha podido actualizar el solicitud de descarga", "error");
+        }
+    }
 
     const handlePageChange = (newSkip) => {
         setSkip(newSkip);
@@ -123,9 +115,9 @@ export default function DownloadRequests() {
                         <TableBody>
                             {downloadrequests.map((downloadrequest) => (
                                 <TableRow key={downloadrequest._id}>
-                                    <TableCell>{downloadrequest.user.name}</TableCell>
-                                    <TableCell>{downloadrequest.user.lastname}</TableCell>
-                                    <TableCell>{downloadrequest.user.email}</TableCell>
+                                    <TableCell>{downloadrequest.researcher?.user?.name}</TableCell>
+                                    <TableCell>{downloadrequest.researcher?.user?.lastname}</TableCell>
+                                    <TableCell>{downloadrequest.researcher?.user?.email}</TableCell>
                                     <TableCell>{downloadrequest.status}</TableCell>
                                     {/* <TableCell>{downloadrequest.occupation}</TableCell>
                                     <TableCell>{downloadrequest.area}</TableCell>
@@ -140,22 +132,27 @@ export default function DownloadRequests() {
                                         >
                                             Ver detalle
                                         </Button>
-                                        <Button
-                                            variant="outlined"
-                                            color="secondary"
-                                            onClick={() => handleUpdateDownloadRequestStatus(downloadrequest._id, downloadrequest.user.state === BLOQUED_USER_STATUS ? ACTIVE_USER_STATUS : BLOQUED_USER_STATUS)}
-                                            sx={{ mr: 1, mb: 1, textTransform: 'none', fontSize: '0.875rem' }}
-                                        >
-                                            {downloadrequest.user.state === BLOQUED_USER_STATUS ? "Desbloquear" : "Bloquear"}
-                                        </Button>
-                                        <Button
-                                            variant="outlined"
-                                            color="secondary"
-                                            onClick={() => handleUpdateDownloadRequestStatus(downloadrequest._id)}
-                                            sx={{ mr: 1, mb: 1, textTransform: 'none', fontSize: '0.875rem' }}
-                                        >
-                                            Aceptar solicitud
-                                        </Button>
+                                        {downloadrequest.status === "Pendiente" ?
+                                            <>
+                                                <Button
+                                                    variant="outlined"
+                                                    color="secondary"
+                                                    onClick={() => handleAcceptRequest(downloadrequest._id)}
+                                                    sx={{ mr: 1, mb: 1, textTransform: 'none', fontSize: '0.875rem' }}
+                                                >
+                                                    Aceptar solicitud
+                                                </Button>
+                                                <Button
+                                                    variant="outlined"
+                                                    color="secondary"
+                                                    onClick={() => handleDennyRequest(downloadrequest._id)}
+                                                    sx={{ mr: 1, mb: 1, textTransform: 'none', fontSize: '0.875rem' }}
+                                                >
+                                                    Denegar solicitud
+                                                </Button>
+                                            </> : <></>
+                                        }
+
                                     </TableCell>
 
                                 </TableRow>
